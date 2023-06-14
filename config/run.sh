217ff571e2b62992b2cdcddea73e84f7dccfabcf
Arvinder Singh Kandola <askandola2@gmail.com>
2023-06-14 23:29:21 +0530

    Update run.sh

--- config/run.sh
+++ config/run.sh
@@ -1,4 +1,3 @@
-python manage.py makemigrations
 python manage.py migrate
-python manage.py collectstatic
+python manage.py collectstatic --noinput
 daphne -e ssl:4376:privateKey=privkey.pem:certKey=cert.pem config.asgi:application
